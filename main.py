944a8ef8f756cb71d5d5e8d80f70a5694a8507d0
Женя Войняк <vojnyak02@mail.ru>
2023-11-17 15:20:00 +0300

    Тактовый синхрогенератор версия1

---
+++ main.py
@@ -0,0 +1,57 @@
+import time
+
+import paho.mqtt.client as mqtt
+from paho.mqtt.client import Client
+import asyncio
+
+# Настройки подключения MQTT
+mqtt_broker = "192.168.0.109"
+mqtt_port = 1883
+mqtt_topic = "topik"
+mqtt_topic2 = "topik"
+
+msg_out = '--'
+client = mqtt.Client()
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Нет подключения " + str(rc))
+
+
+client.connect(mqtt_broker, mqtt_port, 60)
+
+client.subscribe(mqtt_topic2)
+
+
+def on_message(client, userdata, msg):
+    global msg_out
+    msg_out = str(msg.payload.decode())
+
+
+client.on_message = on_message
+client.on_connect = on_connect
+
+s: int = int(input("Введите количество циклов:"))
+print(s)
+start_time1 = time.time()
+for k in (0, s):
+    cklnumber = 0
+
+    while cklnumber < 59:
+
+        client.loop()
+
+        if msg_out != '---':
+
+            client.publish(mqtt_topic, cklnumber)
+            cklnumber += 1
+            msg_out = '---'
+            print(cklnumber)
+        elif msg_out == '---':
+            start_time = time.time()
+            if time.time() - start_time > 5:
+                print('timeout')
+
+
+print(time.time() - start_time1)
+print("fps-", s * 59 / (time.time() - start_time1))
