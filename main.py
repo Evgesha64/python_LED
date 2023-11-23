b6da5ceaf03b0ccfad58893ae0527a1fb1782923
Женя Войняк <vojnyak02@mail.ru>
2023-11-23 17:04:56 +0300

    Добавил отправку RGB

--- main.py
+++ main.py
@@ -27,19 +27,26 @@ client.subscribe(mqtt_topic3)
 def on_message(client, userdata, msg):
     global msg_out
     msg_out = str(msg.payload.decode())
-
+print("Сообщение получено ",msg_out)
 
 client.on_message = on_message
 client.on_connect = on_connect
 
 s: int = int(input("Введите количество циклов:"))
 t: int = int(input("Введите количество тактов:"))
-t -= 1
+
+
 print(s)
 print(t)
 start_time1 = time.time()
+f1 = 0
 for k in range(s):
     cklnumber = 0
+    msg = str("dfjhd")
+
+    f1 += 1
+
+
     #print("Цикл пройден:",cklnumber)
     while cklnumber < t:
 
@@ -50,11 +57,37 @@ for k in range(s):
             client.publish(mqtt_topic1, cklnumber)
             cklnumber += 1
             msg_out = '---'
-           # print(cklnumber)
+            print(cklnumber)
         elif msg_out == '---':
             start_time = time.time()
             if time.time() - start_time > 5:
                 print('timeout')
+    string3 = ""
+    for k1 in range(t):
+
+        string = "#{}#".format(k1)
+        string3 = string3 + string
+        for k2 in range(59):
+            if f1 == 0:
+                string2 = "{},{},{},{},".format(k2, 255, 0, 0)
+                string3 = string3 + string2
+            elif f1 == 1:
+                string2 = "{},{},{},{},".format(k2, 0, 255, 0)
+                string3 = string3 + string2
+            elif f1 == 2:
+                string2 = "{},{},{},{},".format(k2, 0, 0, 255)
+                string3 = string3 + string2
+                f1 = 0
+        if f1 == 2:
+           f1 = 0
+    client.publish(mqtt_topic2, string3)
+
+
+
+
+
+
+
 
 
 print(time.time() - start_time1)
